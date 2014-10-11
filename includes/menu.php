85b4f8d80f7de714274ede9e7e15f3b4a8b27bce
fersoftware <fersoftware@gmail.com>
2014-10-11 15:00:16 -0300

    Dando vida pro menu

--- includes/menu.php
+++ includes/menu.php
@@ -2,11 +2,11 @@
     <div class="navbar-inner">
         <div class="container">
             <ul class="nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Empresa</a></li>
-                <li><a href="#">Produtos</a></li>
-                <li><a href="#">Serviços</a></li>
-                <li><a href="#">Contatos</a></li>
+                <li class="active"><a href="?p=home">Home</a></li>
+                <li><a href="?p=empresa">Empresa</a></li>
+                <li><a href="?p=produtos">Produtos</a></li>
+                <li><a href="?p=servicos">Serviços</a></li>
+                <li><a href="?p=contatos">Contatos</a></li>
             </ul>
         </div>
     </div>
