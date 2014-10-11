7600ec5b52d3efc5432e41529e403be208871bd4
fersoftware <fersoftware@gmail.com>
2014-10-11 14:44:22 -0300

    foi criado uma forma de trocar as paginas e evitar erros da parte do usuario

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+
+$pagina = $_GET['p'];
+
+?>
 <!doctype html>
 <html lang="pt">
 <head>
@@ -13,7 +18,23 @@
         <? require_once('includes/menu.php'); ?>
     </div>
 
-    <? require_once('includes/home.php'); ?>
+    <?
+    // Se a variavel existir ou não estiver em branco mostra a página escolhida, caso contrario
+    // mostra a pagina home por padrao
+    if(!isset($pagina))
+    {
+        $pagina = 'home';
+    }
+
+    //Se a pagina nao existir, exibe que a pagina nao foi encontrada
+    if(!file_exists('includes/'.$pagina.'.php'))
+    {
+       $pagina = 'pagina-nao-encontrada';
+    }
+
+    require_once('includes/'.$pagina.'.php');
+
+     ?>
 
     <hr>
     <? require_once('includes/rodape.php'); ?>
